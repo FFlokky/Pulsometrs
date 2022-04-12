d0e769d3295c0fdc13ca9ce0c11570e2f06800fc
Nazar <nazik.melnichuk@gmail.com>
2022-04-12 22:25:51 +0200

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -20,3 +20,40 @@ document.querySelector('.prev').addEventListener('click', function () {
 document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next')
 });
+
+const tabsNav  = document.querySelectorAll('.catalog__tab');
+const tabsItem = document.querySelectorAll('.catalog__content');
+
+tabsNav.forEach(onTabClick);
+
+function onTabClick(item){
+    item.addEventListener('click', function(){
+        let currentNav = item;
+        let tabId = currentNav.getAttribute('data-tab');
+        let currentTab = document.querySelector(tabId);
+        
+        tabsNav.forEach(function(item){
+            item.classList.remove('catalog__tab_active');
+        });
+        tabsItem.forEach(function(item){
+            item.classList.remove('catalog__content_active');
+        });
+
+        currentNav.classList.add('catalog__tab_active');
+        currentTab.classList.add('catalog__content_active');
+
+    });
+};
+
+const tabsLink = document.querySelectorAll('.catalog-item__link');
+
+function onLinkClick(item){
+    item.addEventListener('click', function(){
+        let currentLink = item;
+
+        currentLink.classList.add('catalog-item__link_active');
+    });
+};
+tabsLink.forEach(onLinkClick);
+
+
